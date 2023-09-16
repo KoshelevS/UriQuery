4090b0001d72afc7eff0fa279341ae8d9e32baa5
Sergey Koshelev <Sergey.Koshelev@dataart.com>
2023-09-16 14:31:31 +0200

    Create solution and projects for the library

---
+++ UriQueryHelper/UriQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace UriQueryHelper;
+
+public class UriQuery
+{
+
+}
---
+++ UriQueryHelperTests/UriQueryTests.cs
@@ -0,0 +1,15 @@
+namespace UriQueryHelperTests;
+
+public class UriQueryTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
